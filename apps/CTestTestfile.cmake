edd3acb0febf44f2ae5fa8a9fb5227ff1995615f
SresserS <1655579665@qq.com>
2022-03-04 20:32:25 +0800

    wkn

---
+++ apps/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wangkn/KF/apps
+# Build directory: /home/wangkn/KF/apps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
