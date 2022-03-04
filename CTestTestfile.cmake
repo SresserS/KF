5911d56cb0cfa68157c77af81d94cfbfeabcd343
yimdx <1595732817@qq.com>
2022-03-04 19:42:13 +0800

    TdfHc added;Replaylocalmarket changed;

---
+++ CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/lixf/Kuafu
+# Build directory: /home/lixf/Kuafu
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
+subdirs("apps")
